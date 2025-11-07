550875dd41d8a36e86a417d2b8ec3b1ce7093217
Ovirne <balantaovirne@gmail.com>
2025-11-07 09:46:35 -0500

    porbando examen final de laravel

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::resource('productos', ProductoController::class);
